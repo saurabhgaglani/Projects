144e8fc09106ab4fa2f1d585f7c336c41b28f5ce
saurabhgaglani <saurabhgaglani@users.noreply.github.com>
2020-08-11 20:01:43 +0100

    Animations+LevelSwitch

---
+++ 2DRPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed; 
+    private Animator anim;
+    private bool IsMoving;
+    private Vector2 MoveDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsMoving = false;
+
+        if(Input.GetAxisRaw("Horizontal")>0 || Input.GetAxisRaw("Horizontal")<0 )
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed *Time.deltaTime, 0f, 0f));
+            MoveDir = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            IsMoving = true;
+        }
+
+        else if(Input.GetAxisRaw("Vertical")<0 || Input.GetAxisRaw("Vertical")>0  )
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed *Time.deltaTime, 0f));
+            MoveDir = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            IsMoving = true;
+        }
+
+
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", IsMoving);
+        anim.SetFloat("LastMoveX", MoveDir.x);
+        anim.SetFloat("LastMoveY", MoveDir.y);
+
+    
+    }
+}
---
+++ 2DRPG/Assets/Scripts/Switch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public string LevelName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [System.Obsolete]
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+            Application.LoadLevel(LevelName);
+            
+        
+        
+    }
+}
